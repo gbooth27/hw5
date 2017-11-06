10902e2e328e2341c2b9c22573a993196cd2643a
grahambooth <gbooth@middlebury.edu>
2017-11-06 14:39:44 -0500

    get the csv reader set up

--- hw5.py
+++ hw5.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 
 def get_data(file):
     """
@@ -6,3 +7,10 @@ def get_data(file):
     :param file:
     :return:
     """
+    with open(file, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            print(row)
+
+if __name__ == '__main__':
+    get_data("country.csv")
