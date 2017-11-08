6dbf696b6ada8ff17d749c4d7ddb6136aee56908
jonahsimon36 <jonahsimon36@gmail.com>
2017-11-08 13:52:22 -0500

    stuff

--- hw5.py
+++ hw5.py
@@ -1,6 +1,14 @@
 import csv
 import numpy as np
 
+def main():
+    print("running")
+    data = get_data("country.csv")
+    print(data)
+    print(len(data[1]))
+    print(sq_distance(data[2],data[4]))
+
+
 def get_data(file):
     """
     Extract the data from the csv file
@@ -9,8 +17,22 @@ def get_data(file):
     """
     with open(file, newline='') as csvfile:
         reader = csv.reader(csvfile)
+        data = []
         for row in reader:
-            print(row)
+            try:
+                data.append(np.array(row[1:], dtype=float))
+            except ValueError:
+                print("row 1")
+        return data
 
-if __name__ == '__main__':
-    get_data("country.csv")
+def sq_distance(x,y):
+    # NEED TO FIX
+    dist = 0
+    for i in len(x):
+        dist += (x[i]*y[i]) * (x[i]*y[i])
+    return dist
+
+
+
+if __name__ == "__main__":
+    main()
