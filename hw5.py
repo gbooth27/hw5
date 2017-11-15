154e7b1711897a7e408c3170b996d2ba56853290
grahambooth <gbooth@middlebury.edu>
2017-11-14 20:34:01 -0500

    clean up code

--- hw5.py
+++ hw5.py
@@ -5,9 +5,19 @@ def main():
     data = get_data("country.csv")
     #print(len(data[1]))
     #print(sq_distance(a,b))
-    centroids = set_centroids(data, 5)
+    centroids = set_centroids(data, 3)
+    converged = False
+    cost = 0
+    while(not converged):
+        prev = cost
+        centroids, cost  = update_centroids(centroids, data, cluster(centroids, data))
+        if cost == prev:
+            converged = True
+
+
+
     for i in range(16):
-        print("update ", i+1)
+        #print("update ", i+1)
         update_centroids(centroids, data, cluster(centroids,data))
 
 
@@ -29,10 +39,10 @@ def get_data(file):
 
 def sq_distance(x,y):
     """
-    Calculate the sqaured distance between two vectors
+    Calculate the squared distance between two vectors
     :param x: first vector
     :param y: second vector
-    :return:
+    :return: squared distance
     """
     dist = np.linalg.norm(x-y)**2
     return dist
@@ -52,31 +62,32 @@ def set_centroids(data,k):
 def cluster(centroids, data):
     """
     Generate a list for each data row that tells us which centroid it is closest to
-    :param centroids:
-    :param data:
-    :return:
+    :param centroids: list of current centroids
+    :param data: data to cluster the centroids
+    :return: list of which centroid each data point belongs to.
     """
     clusters = [0 for _ in range(len(data))]
     for i in range(len(data)):
-        for j in range(1,len(centroids)):
+        for j in range(len(centroids)):
             # compare distances of current centroid to previous
+            # if it is smaller update it.
             if sq_distance(data[i],centroids[j]) < sq_distance(data[i], centroids[clusters[i]]):
                 clusters[i] = j
     return clusters
 
 def update_centroids(centroids, data, clusters):
     """
-    calculate the average location for each cluster
-    :param centroids:
-    :param data:
-    :param cluster:
-    :return:
+     Update the centroids and calculate the average location for each cluster
+    :param centroids: list of centroids to update
+    :param data: data for clustering
+    :param cluster: which data point goes with which cluster
+    :return: updated centroids
     """
     cost = 0
     for i in range(len(centroids)):
         tmp_centroid = np.array([0 for _ in range (len(centroids[i]))])
         num_added = 0
-
+        #
         for j in range(len(data)):
             if clusters[j] == i:
                 tmp_centroid = np.add(tmp_centroid, data[j])
@@ -86,8 +97,8 @@ def update_centroids(centroids, data, clusters):
         for k in range(len(data)):
             if clusters[k] == i:
                 cost += sq_distance(data[k], centroids[i])
-    print("cost for update: {}\n".format( cost/len(data)))
-    return centroids
+    print("cost for update: {}".format( cost/len(data)))
+    return centroids, cost
 
 if __name__ == "__main__":
     main()
